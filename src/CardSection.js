5d2a7cfd8f6d010b06732d99454b8ab657aecf83
harsh021102 <harsh021102@gmail.com>
2022-04-30 17:43:51 +0530

    Removing Comments CardSection

--- src/CardSection.js
+++ src/CardSection.js
@@ -5,9 +5,7 @@ import './CardSection.css'
 function CardSection() {
   return (
     <div className='card__section'>
-       {/* <Scrollbars style={{ width: 500, height: 300 }}> */}
         <CardContainer/>
-      {/* </Scrollbars> */}
     </div>
   )
 }
